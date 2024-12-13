910be05bb1e60588b622008515389b68232b5fca
Iván Salas Aedo <ivanaureliosalasaedo@gmail.com>
2024-12-13 00:05:36 -0300

    updates again

--- script.js
+++ script.js
@@ -6,34 +6,35 @@ tinymce.init({
   height: 600,
   menubar: false,
   plugins: 'link image code table lists',
-  toolbar: 'undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code'
-});
-
-function cargarHTMLBase() {
-    if(!htmlBase) {
-      console.log('Iniciando la carga del template...');
-      fetch('template.html')
-        .then(response => {
-          console.log('Recibida respuesta del servidor:', response);
-          return response.text();
-        })
-        .then(text => {
-          console.log('Texto obtenido:', text.slice(0, 100) + '...'); // Muestra solo los primeros 100 caracteres
-          htmlBase = text;
-          console.log('Asignado htmlBase, estableciendo contenido en TinyMCE');
-          tinymce.get('editor').setContent(htmlBase);
-          console.log('Contenido establecido correctamente en TinyMCE');
-        })
-        .catch(error => {
-          console.error('Error al cargar el template:', error);
-          alert('Error al cargar el template: ' + error);
-        });
-    } else {
-      console.log('htmlBase ya cargado previamente, estableciendo contenido en TinyMCE directamente');
-      tinymce.get('editor').setContent(htmlBase);
-    }
+  toolbar: 'undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code',
+  setup: function(editor) {
+    editor.on('init', function() {
+      console.log('Editor inicializado, iniciando carga del template...');
+      if(!htmlBase) {
+        console.log('Iniciando la carga del template...');
+        fetch('template.html')
+          .then(response => {
+            console.log('Recibida respuesta del servidor:', response);
+            return response.text();
+          })
+          .then(text => {
+            console.log('Texto obtenido:', text.slice(0, 100) + '...');
+            htmlBase = text;
+            console.log('Asignado htmlBase, estableciendo contenido en TinyMCE');
+            tinymce.get('editor').setContent(htmlBase);
+            console.log('Contenido establecido correctamente en TinyMCE');
+          })
+          .catch(error => {
+            console.error('Error al cargar el template:', error);
+            alert('Error al cargar el template: ' + error);
+          });
+      } else {
+        console.log('htmlBase ya cargado previamente, estableciendo contenido en TinyMCE directamente');
+        tinymce.get('editor').setContent(htmlBase);
+      }
+    });
   }
-  
+});
 
 function copiarHTML() {
   var htmlActual = tinymce.get('editor').getContent();
